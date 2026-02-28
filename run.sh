d05e3ca0839b7e908b4f0abd1f304b5d3662c8c6
Lexy Plt <lexplt.dev@gmail.com>
2026-02-28 16:13:38 +0000

    Create /tmp/playground before running the app when running the websocket with run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 if [ -f log.txt ]; then
-	count=$(ls -l . | egrep -c '^-.+log-\d+\.txt')
+	count=$(ls -l . | egrep -c '^-.+log-.+\.txt')
 	mv log.txt log-$count.txt
 fi
 
+mkdir -p /tmp/playground
 node app.js |& tee log.txt
